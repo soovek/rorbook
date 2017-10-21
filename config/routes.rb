fa8ab96683fe6f1e0c7bb05cd5fbbc13128fb109
soovek <pawel.sowinski.1989@gmail.com>
2017-10-21 00:27:53 +0000

    trying to fix Heroku logout issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users do
-    get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
-  end
+  devise_for :users 
+  #do
+  #  get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
+  #end
   
   root 'home#index'
   get 'home/about'
